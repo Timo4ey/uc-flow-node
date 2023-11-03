c5f7e03a973a7911b9d5fdb8c2656ea85b8032a5
root <tima_yakovishin@mail.ru>
2023-11-03 10:24:29 +0000

    [feature] The second impl off the task3

--- node/service.py
+++ node/service.py
@@ -7,47 +7,79 @@ from uc_flow_schemas import flow
 from uc_flow_schemas.flow import Property, CredentialProtocol, RunState
 from uc_http_requester.requester import Request
 from uc_rest.ui.schemas import OptionValue
+from node.enums.enums import Field1, Field2
 
-from node.models.models import SumUp
-
+from node.models.models import  TwoFields
 
 
 class NodeType(flow.NodeType):
     id: str = "62a371e4-099a-4f46-931b-5e3a44727840"
     type: flow.NodeType.Type = flow.NodeType.Type.action
-    name: str = "SumUp"
-    displayName: str = "SumUp"
-    icon: str = '<svg><text x="8" y="50" font-size="50">🥸</text></svg>'
-    description: str = "Example of SumUp"
+    name: str = "Switcher"
+    displayName: str = "Switcher"
+    icon: str = '<svg><text x="8" y="50" font-size="50">🪪</text></svg>'
+    description: str = "Example of Switcher"
     properties: List[Property] = [
-        Property(
-            displayName="Введите число",
-            name="num_1",
-            type=Property.Type.STRING,
-            placeholder="Введите число в виде строки. Пример: 1",
-            description="Поле принимает строку",
-            required=True,
-            default="1",
-        ),
-        Property(
-            displayName="Введите число",
-            name="num_2",
-            type=Property.Type.NUMBER,
-            placeholder="Введите число. Пример: 2",
-            description="Поле принимает число",
-            required=True,
-            default=2,
-        ),
         Property(
             displayName="Переключатель",
             name="switcher",
-            type=Property.Type.OPTIONS,
+            type=Property.Type.BOOLEAN,
+            required=True,
+            default=0,
+        
+        ),
+
+        Property(
+            displayName="Поле 1",
+            name=Field1.field_name,
+            noDataExpression=True,
+            type=Property.Type.MULTI_OPTIONS,
             options=[
-                OptionValue(name="Число", value="number"),
-                OptionValue(name="Строка", value="string"),
+                OptionValue(name="Значение 1", value=Field1.value_1),
+                OptionValue(name="Значение 2", value=Field1.value_2),
             ],
             description="Выберите тип возвращаемых данных",
-            required=True,
+            displayOptions=flow.DisplayOptions(show={
+                'switcher': [True]
+            })
+        ),
+
+        Property(
+            displayName="Поле 2",
+            name=Field2.field_name,
+            noDataExpression=True,
+            type=Property.Type.MULTI_OPTIONS,
+            options=[
+                OptionValue(name="Значение 1", value=Field2.value_1),
+                OptionValue(name="Значение 2", value=Field2.value_2),
+            ],
+            description="Выберите тип возвращаемых данных",
+            displayOptions=flow.DisplayOptions(show={
+                'switcher': [True]
+            })
+        ),
+
+        Property(
+            displayName="“Значение 1”: Поле для ввода почты",
+            name="email",
+            noDataExpression=True,
+            type=Property.Type.EMAIL,
+            displayOptions=flow.DisplayOptions(show={
+                'switcher': [True],
+                'field_1': ['field_1_1'],
+                'field_2': ['field_2_1'],
+            })
+        ),
+        Property(
+            displayName="“Значение 2”: Поле для ввода даты и времени",
+            name="date",
+            noDataExpression=True,
+            type=Property.Type.DATETIME,
+            displayOptions=flow.DisplayOptions(show={
+                'switcher': [True],
+                'field_1': ['field_1_2'],
+                'field_2': ['field_2_2']
+            })
         ),
     ]
 
@@ -61,18 +93,13 @@ class ExecuteView(execute.Execute):
     async def post(self, json: NodeRunContext) -> NodeRunContext:
         try:
             properties = json.node.data.properties
-            sum_up_model = SumUp(
-                num_1=properties.get("num_1"),
-                num_2=properties.get("num_2"),
-                switcher=properties.get("switcher"),
-            )
-            result = sum_up_model.sum_of_digits()
+            result = TwoFields(switcher=properties.get('switcher'),
+                      email=properties.get('email'),
+                      date=properties.get('date'),
+                      )
+   
+            await json.save_result({"result": result.json()})
 
-            match sum_up_model.switcher:
-                case "number":
-                    await json.save_result({"result": result})
-                case "string":
-                    await json.save_result({"result": str(result)})
 
             json.state = RunState.complete
         except Exception as e:
